15a6b85b5c852daeef3107ea166037c0d3fca306
Helewidis <108878708+Helewidis@users.noreply.github.com>
2022-11-18 07:41:46 +0900

    FEAT: likes API

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 
 const userRouter = require('./user');
 const movieRouter = require('./movieRouter');
+const likesRouter = require('./likesRouter');
+
 // const otherRouter = require('./other');
 
 const router = express.Router();
 
 // router.use(userRouter);
 router.use('/movie', movieRouter);
+router.use('/likes', likesRouter);
+
 // router.use(otherRouter);
 
 module.exports = router;
