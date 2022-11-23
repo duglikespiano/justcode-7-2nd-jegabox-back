f0e44fc77b31533951229979be8c1d6258b3ac35
kihyeonKim <kidone0033@gmail.com>
2022-11-23 14:35:50 +0900

    feat/예매 기록 확인, 예매 취소 기록 확인, 헤더 API 구현

--- models/mypageDAO.js
+++ models/mypageDAO.js
@@ -1,14 +1,93 @@
 const database = require('./database');
 
-async function getBookRecord(user_id) {
+async function getBookList(user_id) {
   const rtn = await database.query(`
     SELECT
-        *
+	    ticket_number,
+      ko_title,
+      movie_property,
+      movie_poster,
+      cinema_name,
+      screen,
+      seat_count_adult,
+      seat_count_child,
+      GROUP_CONCAT(booking.seat_name) as seat_name,
+      showtime_day,
+      start_time,
+      created_at
     FROM
-        booking
-    WHERE user_id = '${user_id}'
+	    booking
+    WHERE
+      user_id = ${user_id}
+    GROUP BY
+	    ticket_number,
+      ko_title, movie_property,
+      movie_poster, cinema_name,
+      screen, seat_count_adult,
+      seat_count_child,
+      showtime_day,
+      start_time,
+      created_at;
   `);
   return rtn;
 }
 
-module.exports = { getBookRecord };
+async function getCancelList(user_id) {
+  const rtn = await database.query(`
+    SELECT
+      *
+    FROM
+      canceled_booking
+    WHERE
+      canceled_booking.user_id = ${user_id}
+  `);
+  return rtn;
+}
+
+async function ExistBookingRecord(user_id) {
+  const result = await database.query(`
+    SELECT
+      *
+    FROM
+      booking
+    WHERE
+      user_id = ${user_id}
+  `);
+  return result;
+}
+
+async function getHeaderInfoWithBook(user_id) {
+  const [result] = await database.query(`
+    SELECT DISTINCT
+      user.name,
+      booking.ko_title,
+      booking.created_at
+    FROM
+      user
+    JOIN booking ON booking.user_id = user.id
+    WHERE
+      user.id = ${user_id}
+    ORDER BY created_at desc
+  `);
+  return result;
+}
+
+async function getHearderInfoWithoutBook(user_id) {
+  const rtn = await database.query(`
+    SELECT
+      user.name
+    FROM
+      user
+    WHERE
+      user.id = ${user_id}
+  `);
+  return rtn;
+}
+
+module.exports = {
+  getBookList,
+  getCancelList,
+  getHeaderInfoWithBook,
+  ExistBookingRecord,
+  getHearderInfoWithoutBook,
+};
